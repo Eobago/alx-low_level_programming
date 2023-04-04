b75868b5d8d883168584cf07fdbde7ada8bfd714
Eobago <emmanuelobasi159@gmail.com>
2023-04-03 19:24:45 -0700

    Add file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,24 +2,22 @@
 #define NULL 0
 
 /**
- * _strchr - locate 1st  occurrence of char in string and returns pointer there
- * @s: strings to search
- * @c: target character
- * Return: pointer to that charcter in string
- */
+* _strchr - locate 1st occurrence of char in string and returns pointer there
+* @s: string to search
+* @c: target characer
+* Return: pointer to that character in string
+*/
 
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char c)
 {
-	int i = 0
-		;
+	int i = 0;
 
 	while (s[i] != '\0' && s[i] != c)
 		i++;
 
 	if (s[i] == c)
-
 		return (&s[i]);
 
 	else
-		return (NULL)
+		return (NULL);
 }
