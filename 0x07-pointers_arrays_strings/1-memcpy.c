e7a590ebed6c043f0a8c842de6ac2769668afd5f
Eobago <emmanuelobasi159@gmail.com>
2023-04-03 18:02:06 -0700

    Add file 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy() -  copies function between two memories
+ * @n: number of bytes
+ * @dest: Destination
+ * @src: Source
+ * Return: pointer  to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	int size = n;
+
+	if (size < 0)
+	{
+		int i;
+		for (i = 0; i < size; i++)
+
+			dest[i] = src[i];
+	}
+	return (dest);
+}
+
