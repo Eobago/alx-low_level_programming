4e8ea3deaf3886c033d9f7455ffd2df51bfe3790
Eobago <emmanuelobasi159@gmail.com>
2023-04-05 16:24:04 -0700

    Add file 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* set_string - set value of pointer to a char
+* @s: type pointer vairable
+* @to: char to target
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
