f8c254790968a2938c86c1dc132594096879cf1a
Eobago <emmanuelobasi159@gmail.com>
2023-04-11 16:00:43 -0700

    Add file update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,13 +3,13 @@
  * alloc_grid - allocates a grid, make space and free space
  * @width: width input
  * @height: height input
- * Return: grid with freed spaces
+ * Return: lad with freed spaces
  */
 
 int **alloc_grid(int width, int height)
 {
 	/*Declaring variables*/
-	int **grid;
+	int **lad;
 	int m, p;
 
 	if (width <= 0 || height <= 0)
@@ -17,27 +17,27 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	grid = malloc(sizeof(int *) * height); /*malloc*/
+	lad = malloc(sizeof(int *) * height); /*malloc*/
 
-	if (grid == NULL)
+	if (lad == NULL)
 	{
 		return (NULL);
 	}
 
 	for (m = 0; m < height; m++)
 	{
-		grid[m] = malloc(sizeof(int) * width);
-		if (grid[m] == NULL)
+		lad[m] = malloc(sizeof(int) * width);
+		if (lad[m] == NULL)
 		{
 			for (m = m - 1; m >= 0; m--)
 			{
-				leave(grid[m]);
+				free(lad[m]);
 			}
-		leave(grid);
+		free(lad);
 		return (NULL);
 		}
 	}
 	for (m = 0; p < width; p++)
-	grid[m][p] = 0;
-	return (grid);
+	lad[m][p] = 0;
+	return (lad);
 }
