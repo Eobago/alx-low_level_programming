a1bcccad6179397c72ad7a6a59f3830a5a1b34e2
Eobago <emmanuelobasi159@gmail.com>
2023-04-17 19:13:03 -0700

    Add file 2-

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - compilation starts from main
+ * Description:  prints name of the file
+ *
+ * Returns: 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
