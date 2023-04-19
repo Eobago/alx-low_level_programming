768f07acd0ae9847c1c3e08f29098cd9916068cf
Eobago <emmanuelobasi159@gmail.com>
2023-04-19 06:37:14 -0700

    Add file correction

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_interator - calls a function to acts on an array elements
+ * @array: array
+ * @size: number of element to print
+ * @action: pointer to print in regular or hex
+ * Return: no return void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int p;
+
+	if (!array || !action)
+		return;
+
+	for (p = 0; p < size; p++)
+		action(array[p]);
+}
